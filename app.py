a8cececa7a44ef6ad801ab270221b887e331c8ac
eolnuha <eol.nuha22@gmail.com>
2022-02-25 11:45:32 +0100

    Added 403 and 500 error handlers

--- app.py
+++ app.py
@@ -73,8 +73,16 @@ class Comment(db.Model):
 
 
 @app.errorhandler(404)
-def page_not_found(e):
+def error_404(e):
     return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def error_403(e):
+    return render_template('403.html'), 403
+
+@app.errorhandler(500)
+def error_500(e):
+    return render_template('500.html'), 500
 
 @login_manager.user_loader
 def load_user(user_id):
