100161255477c0970138e189dbf06ca139a743f3
eolnuha <eol.nuha22@gmail.com>
2022-04-22 12:43:11 +0200

    Returned cex ads

--- static/blog/components/banner.js
+++ static/blog/components/banner.js
@@ -4,6 +4,7 @@ Vue.component('banner', {
       <div class="container">
           <div class="row">
               <div class="col-lg-12">
+                <a href="https://cex.io/r/0/up145283861/0/" target="_blank"><div class="cex-banner"></div></a>
               </div>
           </div>
       </div>
