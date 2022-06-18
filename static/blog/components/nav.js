1571bfc087e2af4d82552ae4dd707c648df45ca5
eolnuha <eol.nuha@weborigo.eu>
2022-06-18 13:10:10 +0200

    Fixed the background image

--- static/blog/components/nav.js
+++ static/blog/components/nav.js
@@ -3,14 +3,14 @@ Vue.component('nav-bar', {
     template: `
     <nav class="navbar navbar-expand-lg">
         <div class="container">
-          <a class="navbar-brand" href="/blog"><img src="/static/blog/assets/images/logo-icon-blue.png" width="230px" alt="Eol Nuha Blogs Logo"/></a>
+          <a class="navbar-brand" href="/blog/"><img src="/static/blog/assets/images/logo-icon-blue.png" width="230px" alt="Eol Nuha Blogs Logo"/></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
               <li class="nav-item" :class="{'active': active === 'home'}">
-                <a class="nav-link" href="/blog">Home</a>
+                <a class="nav-link" href="/blog/">Home</a>
               </li>
               <li class="nav-item" :class="{'active': active === 'entries'}">
                 <a class="nav-link" href="/blog/tutorials/">Tutorials</a>
