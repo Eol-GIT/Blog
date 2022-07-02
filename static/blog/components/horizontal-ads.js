4dd96cddda57df047fba3c9563ab0f954848ddeb
eolnuha <eol.nuha@weborigo.eu>
2022-07-02 15:58:34 +0200

    Removed google ads until suspension

--- static/blog/components/horizontal-ads.js
+++ static/blog/components/horizontal-ads.js
@@ -1,11 +1,11 @@
-Vue.component('horizontal-ads', {
-    template: `
-     <ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-8400632007706493"
-     data-ad-slot="3731422913"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-    `
-})
-new Vue({el: "#horizontal-ads"});
+// Vue.component('horizontal-ads', {
+//     template: `
+//      <ins class="adsbygoogle"
+//      style="display:block"
+//      data-ad-client="ca-pub-8400632007706493"
+//      data-ad-slot="3731422913"
+//      data-ad-format="auto"
+//      data-full-width-responsive="true"></ins>
+//     `
+// })
+// new Vue({el: "#horizontal-ads"});
--- static/blog/components/sidebar-ads.js
+++ static/blog/components/sidebar-ads.js
@@ -1,11 +1,11 @@
-Vue.component('sidebar-ads', {
-    template: `
-    <ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-8400632007706493"
-     data-ad-slot="2615466570"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-    `
-})
-new Vue({el: "#sidebar-ads"});
+// Vue.component('sidebar-ads', {
+//     template: `
+//     <ins class="adsbygoogle"
+//      style="display:block"
+//      data-ad-client="ca-pub-8400632007706493"
+//      data-ad-slot="2615466570"
+//      data-ad-format="auto"
+//      data-full-width-responsive="true"></ins>
+//     `
+// })
+// new Vue({el: "#sidebar-ads"});
