b9ab5ff39b476c121c57399cbcb69bf550ee4a9d
eolnuha <eol.nuha@weborigo.eu>
2022-07-08 13:47:01 +0200

    Added copy text to copy btn

--- static/blog/components/blog-details.js
+++ static/blog/components/blog-details.js
@@ -7,6 +7,7 @@ const CopyPlugin = {
                         node.classList.add('code-copy-btn');
                         let copyBtn = document.createElement("button");
                         copyBtn.setAttribute("title", "Copy");
+                        copyBtn.setAttribute("data-toggle", "tooltip");
                         copyBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                             <path d="M502.6 70.63l-61.25-61.25C435.4 3.371 427.2 0 418.7 0H255.1c-35.35 0-64 28.66-64 64l.0195 256C192 355.4 220.7 384 256 384h192c35.2 0 64-28.8 64-64V93.25C512 84.77 508.6 76.63 502.6 70.63zM464 320c0 8.836-7.164 16-16 16H255.1c-8.838 0-16-7.164-16-16L239.1 64.13c0-8.836 7.164-16 16-16h128L384 96c0 17.67 14.33 32 32 32h47.1V320zM272 448c0 8.836-7.164 16-16 16H63.1c-8.838 0-16-7.164-16-16L47.98 192.1c0-8.836 7.164-16 16-16H160V128H63.99c-35.35 0-64 28.65-64 64l.0098 256C.002 483.3 28.66 512 64 512h192c35.2 0 64-28.8 64-64v-32h-47.1L272 448z" />
                         </svg>`;
@@ -17,11 +18,20 @@ const CopyPlugin = {
                                 let text = node.querySelector(childSelector).innerText;
                                 await navigator.clipboard.writeText(text);
                             }
-                            copyBtn.innerHTML = `<i class="fa fa-check" title="Copied"></i>`;
-                            setTimeout(() => copyBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            copyBtn.innerHTML = `<i class="fa fa-check"></i>`;
+                            $('[data-toggle="tooltip"]').tooltip('dispose');
+                            copyBtn.setAttribute("title", "Copied");
+                            $('[data-toggle="tooltip"]').tooltip()
+                            
+                            setTimeout(function() {
+                                copyBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                 <path d="M502.6 70.63l-61.25-61.25C435.4 3.371 427.2 0 418.7 0H255.1c-35.35 0-64 28.66-64 64l.0195 256C192 355.4 220.7 384 256 384h192c35.2 0 64-28.8 64-64V93.25C512 84.77 508.6 76.63 502.6 70.63zM464 320c0 8.836-7.164 16-16 16H255.1c-8.838 0-16-7.164-16-16L239.1 64.13c0-8.836 7.164-16 16-16h128L384 96c0 17.67 14.33 32 32 32h47.1V320zM272 448c0 8.836-7.164 16-16 16H63.1c-8.838 0-16-7.164-16-16L47.98 192.1c0-8.836 7.164-16 16-16H160V128H63.99c-35.35 0-64 28.65-64 64l.0098 256C.002 483.3 28.66 512 64 512h192c35.2 0 64-28.8 64-64v-32h-47.1L272 448z" />
-                            </svg>`, 2000);
-                        })
+                                </svg>`;
+                                $('[data-toggle="tooltip"]').tooltip('dispose');
+                                copyBtn.setAttribute("title", "Copy");
+                                $('[data-toggle="tooltip"]').tooltip()
+                            }, 2000)
+                        });
                     })
                 }
                 enableCopy("pre"); 
@@ -54,6 +64,22 @@ const HighlightPlugin = {
         Vue.mixin({
             mounted: function () {
                 Prism.highlightAll();
+            }
+        })
+    }
+}
+
+const TooltipPlugin = {
+    install: function (Vue) {
+        Vue.mixin({
+            mounted: function () {
+                var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
+                var tooltipList = tooltipTriggerList.map(function(element){
+                    new bootstrap.Tooltip(element, {
+                        delay: {show: 0, hide: 0}
+                    });
+                    return new bootstrap.Tooltip(element);
+                });
             }
         })
     }
@@ -265,4 +291,5 @@ Vue.component('blog-details', {
 Vue.use(CopyPlugin);
 Vue.use(TableContentsPlugin);
 Vue.use(HighlightPlugin);
+Vue.use(TooltipPlugin);
 new Vue({ el: "#blog-details" });
